3a069d8cd6ac1c08eab36fa72ad831b781fcf5e6
ABDULHADI18 <160162625+ABDULHADI18@users.noreply.github.com>
2024-03-20 23:39:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "Write your guess between 1 to 10:  ",
+    },
+]);
+const { userGuess } = answer;
+console.log(userGuess, "userGuess", systemGeneratedNo, "genetratedNumber");
+if (userGuess === systemGeneratedNo) {
+    console.log(chalk.green("You WON The Game."));
+}
+else {
+    console.log(chalk.red("Better Luck Next Time"));
+}
+console.log("The End");
